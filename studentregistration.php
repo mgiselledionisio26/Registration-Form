6cb36810ce80d48f70e2e76f6313a7185f8f8d14
benjaminjoseph13 <benjaminjosephlongakit@gmail.com>
2015-10-25 20:53:38 +0800

    Registration Form

---
+++ studentregistration.php
@@ -0,0 +1,18 @@
+<?php
+	
+	$firstname = $_POST['f_name'];
+	$middlename = $_POST ['m_name'];
+	$lastname = $_POST ['l_name'];
+	
+?>
+	First Name: <strong> <?php echo $firstname;?> </strong> </br>
+	Middle Name: <strong> <?php echo $middlename;?> </strong></br>
+	Last Name: <strong> <?php echo $lastname;?></strong></br>	
+	
+	
+	
+	
+	
+	
+	
+	
